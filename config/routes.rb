b7adb8b38adeb32eca50e5299b9fd5ddedae69b7
nullspace <nullspace@thinhorn.com>
2011-12-19 22:29:38 -0800

    First couple of changes trying to get this running - not there yet

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :locations
     
-    map.resources :accounts
+    map.resources :account, :collection => {:signup => :get}
 
   # The priority is based upon order of creation: first created -> highest priority.
 
