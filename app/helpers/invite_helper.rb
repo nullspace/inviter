0133630100d0138382e7ebcc319d1b3fad18c6b7
nullspace <nullspace@thinhorn.com>
2012-03-13 21:58:34 -0600

    in middle of some edits - resend link not working

--- app/helpers/invite_helper.rb
+++ app/helpers/invite_helper.rb
@@ -16,7 +16,7 @@ module InviteHelper
         html = "<tr valign='top'><td>#{h(r.person.name)} &lt;#{h(r.person.email)}&gt;</td>"
         html += "<td><i>#{date}</i></td><td>#{r.num or 1}</td>"
         html += "<td>#{h(r.response)}</td><td>#{r.id}</td>"
-        #html += "<td>#{link_to 'Resend', r, :confirm => resend, :method => :post}</td></tr>"
+        html += "<td>#{link_to 'Resend', r, :confirm => resend, :method => :post}</td></tr>"
         html += "<td>#{link_to 'Remove', r, :confirm => uninvite, :method => :delete}</td></tr>"
         return html
     end
